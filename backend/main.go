550883eba23568b7d20dc231e1afba1c983342a1
Filip Ptiček <filip.pt741@gmail.com>
2021-01-05 17:22:58 +0100

    Added comments

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,23 +12,28 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+//Model in table
 type device struct {
 	ID     int    `json:"id"`
 	Status int    `json:"status"`
 	Name   string `json:"name"`
 }
 
+//Struct for multiple devices
 type devices struct {
 	Devs []device `json:"devices"`
 }
 
+//Struct for database mutex control
 type mutexDB struct {
 	mutex    *sync.Mutex
 	sqliteDB *sql.DB
 }
 
+//Handles a PUT request for creating a device
 func (md *mutexDB) createDevice(w http.ResponseWriter, r *http.Request) {
 	var d device
+	//Decode JSON to device struct
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&d)
 	if err != nil {
@@ -38,9 +42,12 @@ func (md *mutexDB) createDevice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Synchronize db for write
 	md.mutex.Lock()
 	defer md.mutex.Unlock()
+
 	log.Println("Creating device")
+	//Create INSERT statement
 	insert := `INSERT INTO devices(id, status, name) VALUES (?, ?, ?)`
 	statement, err := md.sqliteDB.Prepare(insert)
 	if err != nil {
@@ -49,20 +56,25 @@ func (md *mutexDB) createDevice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Execute statement and return status code depending on the success
 	_, err = statement.Exec(d.ID, d.Status, d.Name)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusConflict)
 		return
 	}
+
+	//Header and JSON encoding
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(d)
 }
 
+//Handles a PUT request for updating a device with /{id}
 func (md *mutexDB) updateDevice(w http.ResponseWriter, r *http.Request) {
 	var d device
 	param := mux.Vars(r)
 	id := param["id"]
+	//Decode JSON to device struct
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&d)
 	if err != nil {
@@ -70,9 +82,12 @@ func (md *mutexDB) updateDevice(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	//Synchronize db for write
 	md.mutex.Lock()
 	defer md.mutex.Unlock()
+
 	log.Println("Updating device")
+	//Create UPDATE statement
 	update := `UPDATE devices SET id=?, status=?, name=? WHERE id=?`
 	statement, err := md.sqliteDB.Prepare(update)
 	if err != nil {
@@ -81,23 +96,30 @@ func (md *mutexDB) updateDevice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Execute statement and return status code depending on the success
 	_, err = statement.Exec(d.ID, d.Status, d.Name, id)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+
+	//Header and JSON encoding
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(d)
 }
 
+//Handles a DELETE request for deleting a device with /{id}
 func (md *mutexDB) deleteDevice(w http.ResponseWriter, r *http.Request) {
 	param := mux.Vars(r)
 	id := param["id"]
 
+	//Synchronize db for write
 	md.mutex.Lock()
 	defer md.mutex.Unlock()
+
 	log.Println("Deleting device")
+	//Create DELETE statement
 	del := `DELETE FROM devices WHERE id=?`
 	statement, err := md.sqliteDB.Prepare(del)
 	if err != nil {
@@ -106,6 +128,7 @@ func (md *mutexDB) deleteDevice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Execute statement and return status code depending on the success
 	_, err = statement.Exec(id)
 	if err != nil {
 		log.Println(err)
@@ -115,12 +138,14 @@ func (md *mutexDB) deleteDevice(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+//Handles a GET request for a device with /{id}
 func (md *mutexDB) getDevice(w http.ResponseWriter, r *http.Request) {
 	var d device
 	param := mux.Vars(r)
 	id := param["id"]
 
 	log.Println("Getting device")
+	//Create SELECT query and populate device struct
 	qSelect := `SELECT id, status, name FROM devices WHERE id=?`
 	err := md.sqliteDB.QueryRow(qSelect, id).Scan(&d.ID, &d.Status, &d.Name)
 	if err != nil {
@@ -129,14 +154,16 @@ func (md *mutexDB) getDevice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Header and JSON encoding
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(d)
 }
 
+//Handles a GET request for all devices
 func (md *mutexDB) getEveryDevice(w http.ResponseWriter, r *http.Request) {
-
 	log.Println("Getting all devices")
+	//Create SELECT query
 	qSelect := `SELECT id, status, name FROM devices`
 	rows, err := md.sqliteDB.Query(qSelect)
 	if err != nil {
@@ -144,6 +171,7 @@ func (md *mutexDB) getEveryDevice(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+	//Read rows, create device struct and append to devices struct
 	var dev = devices{Devs: make([]device, 0)}
 	for rows.Next() {
 		var d device
@@ -151,14 +179,15 @@ func (md *mutexDB) getEveryDevice(w http.ResponseWriter, r *http.Request) {
 		dev.Devs = append(dev.Devs, d)
 	}
 
+	//Header and JSON encoding
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(dev)
 }
 
 func main() {
-	fmt.Println("hey")
-	os.Remove("devices.db")
+
+	os.Remove("devices.db")              //Delete db file for a fresh start
 	file, err := os.Create("devices.db") // Create SQLite file
 	if err != nil {
 		log.Fatal(err)
@@ -170,14 +199,18 @@ func main() {
 		log.Fatal(err)
 	}
 	defer sqliteDB.Close()
+	//Create table devices
 	statement, err := sqliteDB.Prepare("CREATE TABLE devices (id INTEGER PRIMARY KEY, status INTEGER CHECK (status IN (0,1)), name TEXT)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	statement.Exec()
+
+	//Initialize mutex and create mutexDB struct
 	var m sync.Mutex
 	mutexDB := &mutexDB{mutex: &m, sqliteDB: sqliteDB}
 
+	//Add routes
 	router := mux.NewRouter()
 	router.HandleFunc("/", mutexDB.getEveryDevice).Methods(http.MethodGet)
 	router.HandleFunc("/", mutexDB.createDevice).Methods(http.MethodPut)
@@ -185,6 +218,5 @@ func main() {
 	router.HandleFunc("/{id}", mutexDB.getDevice).Methods(http.MethodGet)
 	router.HandleFunc("/{id}", mutexDB.deleteDevice).Methods(http.MethodDelete)
 
-	http.ListenAndServe(":8080", router)
-
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
